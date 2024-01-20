44fbf7b5b3e77de01d42331fc272cf4135d9cc08
williamgong2006 <149895342+williamgong2006@users.noreply.github.com>
2024-01-19 23:09:18 -0800

    convert code to a function and use command line parameters

--- project.py
+++ project.py
@@ -1,43 +1,45 @@
-import sys
+import argparse
 
-if len(sys.argv) != 6:
-    print("Usage: python script.py sequence_file.txt sequence2_file.txt Pos Ref Alt")
-    sys.exit(1)
-
-# Read sequence from the first command line argument
-sequence_filename = sys.argv[1]
-with open(sequence_filename, "r") as file:
-    sequence = file.read().strip()
-
-# Read sequence2 from the second command line argument
-sequence2_filename = sys.argv[2]
-with open(sequence2_filename, "r") as file:
-    sequence2 = file.read().strip()
-
-# Parse Pos, Ref, and Alt from command line arguments
-Pos = int(sys.argv[3])
-Ref = sys.argv[4]
-Alt = sys.argv[5]
-
-# code for updating Pos based on conditions
-if sequence[Pos - 3:Pos - 1] != "CA":
-    Pos = 8
-else:
-    if sequence[Pos - 5:Pos - 3] != "CA":
+def left_justify_variant_calls(sequence, Pos, Ref, Alt):
+    # Code for updating Pos based on conditions
+    if sequence[Pos - 3:Pos - 1] != "CA":
+        Pos = 8
+    elif sequence[Pos - 5:Pos - 3] != "CA":
         Pos = 6
+    elif sequence[Pos - 7:Pos - 5] != "CA":
+        Pos = 4
     else:
-        if sequence[Pos - 7:Pos - 5] != "CA":
-            Pos = 4
-        else:
-            Pos = 2
+        Pos = 2
 
-# code for updating Ref and Alt based on conditions
-if Alt == "":
-    Pos -= 1
-    Ref = sequence[Pos - 1] + Ref
-    Alt = sequence[Pos - 1] + Alt
+    # Code for updating Ref and Alt based on conditions
+    if Alt == "":
+        Pos -= 1
+        Ref = sequence[Pos - 1] + Ref
+        Alt = sequence[Pos - 1] + Alt
 
-# Print the updated values
-print("Updated Pos:", Pos)
-print("Updated Ref:", Ref)
-print("Updated Alt:", Alt)
+    return Pos, Ref, Alt
+
+def main():
+    parser = argparse.ArgumentParser(description='Perform left justification of variant calls.')
+    parser.add_argument('sequence_file', type=str, help='Path to the sequence file')
+    parser.add_argument('Pos', type=int, help='Position (integer) for the variant call')
+    parser.add_argument('Ref', type=str, help='Reference sequence')
+    parser.add_argument('Alt', type=str, help='Alternate sequence')
+
+    args = parser.parse_args()
+
+    # Read sequence from the first command line argument
+    with open(args.sequence_file, "r") as file:
+        sequence = file.read().strip()
+
+    # Call the function to perform left justification
+    updated_Pos, updated_Ref, updated_Alt = left_justify_variant_calls(sequence, args.Pos, args.Ref, args.Alt)
+
+    # Print the updated values
+    print("Updated Pos:", updated_Pos)
+    print("Updated Ref:", updated_Ref)
+    print("Updated Alt:", updated_Alt)
+
+if __name__ == "__main__":
+    main()
+
